d37cd7e3ac98ec9fe337bcbaf5591309e5dad79d
lautaro <lautaroolguin89@gmail.com>
2023-11-12 19:27:57 -0300

    agregando import

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Navigation from './navigation';
+import Login from './pantallas/login';
 
 export default function App() {
   return (
-    <Navigation />
+    <Login />
   )
 
 }
---
+++ pantallas/login.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
