27aa14d9ecbeb0eb852a152034ebafc5299ed58e
joaquin <joaquineaceituno@gmail.com>
2023-11-12 18:43:01 -0300

    dependencias para iconos y navegacion

--- navigation.js
+++ navigation.js
@@ -3,3 +3,6 @@ import boton from "./pantallas/boton";
 import inicio from "./pantallas/inicio";
 import mapa from "./pantallas/mapa";
 import { View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
