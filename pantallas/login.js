36702ca276d5132c5240cb9ad083113e36b476dd
lautaro <lautaroolguin89@gmail.com>
2023-11-12 19:29:00 -0300

    agaregando export

--- pantallas/login.js
+++ pantallas/login.js
@@ -4,3 +4,16 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
+
+export default function Login() {
+    return (
+  });
+  
+  // Función para manejar la acción de iniciar sesión
+  function handleLogin() {
+    // Aquí puedes implementar la lógica de inicio de sesión
+    // Por ejemplo, puedes navegar a la pantalla de inicio después de iniciar sesión.
+  }
+  
+  
