71c1cf8b69774fc1af939e19e51e0fe249f6f6fc
joaquin <joaquineaceituno@gmail.com>
2023-11-12 18:24:54 -0300

    se importan dependencias

---
+++ navigation.js
@@ -0,0 +1,5 @@
+import React from "react";
+import boton from "./pantallas/boton";
+import inicio from "./pantallas/inicio";
+import mapa from "./pantallas/mapa";
+import { View, Text, StyleSheet } from "react-native";
