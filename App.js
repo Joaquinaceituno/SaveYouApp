088f80b2c91d7c394e7ddd1b3a047dbfd5e13a9a
lautaro <lautaroolguin89@gmail.com>
2023-11-12 19:32:06 -0300

    agaregando el login a app

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './navigation';
 import Login from './pantallas/login';
 
 export default function App() {
