bfa3f253da52f266bd5d5ce66e6e07da3c0fa3b2
Pupghst <xiararojido04@gmail.com>
2023-11-13 18:45:05 -0300

    Agregando return en la pantalla de registro

--- pantallas/registro.js
+++ pantallas/registro.js
@@ -5,9 +5,13 @@ export default function registro(){
   const [nombre, setNombre] = useState('');
   const [email, setEmail] = useState('');
   const [contrasena, setContrasena] = useState('');
-  
+
    const handleRegistro = () => {
     console.log('Nombre:', nombre);
     console.log('Email:', email);
     console.log('Contraseña:', contrasena);
   };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Registro</Text>
