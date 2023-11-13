11cce2f6f2cd6a5547edd1300376bab41e470158
Pupghst <xiararojido04@gmail.com>
2023-11-13 18:40:25 -0300

    Haciendo export and const's

--- pantallas/registro.js
+++ pantallas/registro.js
@@ -1,2 +1,13 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+export default function registro(){
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [contrasena, setContrasena] = useState('');
+  
+   const handleRegistro = () => {
+    console.log('Nombre:', nombre);
+    console.log('Email:', email);
+    console.log('Contraseña:', contrasena);
+  };
