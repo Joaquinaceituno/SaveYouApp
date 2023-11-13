8b5981b4c35ffc5024b6828dd48f176467aa5119
Pupghst <xiararojido04@gmail.com>
2023-11-13 18:48:24 -0300

    Haciendo cons de estilos para la pantalla de registro

--- pantallas/registro.js
+++ pantallas/registro.js
@@ -45,3 +45,36 @@ export default function registro(){
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingLeft: 10,
+  },
+  boton: {
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  textoBoton: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
