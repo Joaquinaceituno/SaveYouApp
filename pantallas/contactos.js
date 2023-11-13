b3fdebc9ee2967fe624fa09f089d8d1216e242dd
Pupghst <xiararojido04@gmail.com>
2023-11-13 19:34:04 -0300

    Modificando en la pantalla de contactos

--- pantallas/contactos.js
+++ pantallas/contactos.js
@@ -60,6 +60,7 @@ const styles = StyleSheet.create({
       fontSize: 24,
       fontWeight: 'bold',
       marginBottom: 20,
+      textAlign: 'center',
     },
     formulario: {
       width: '100%',
