9998bf5f920ef0f1c6331f7b908e77ae93715988
Pupghst <xiararojido04@gmail.com>
2023-11-13 19:07:49 -0300

    Haciendo const's en la pantalla de contactos

--- pantallas/contactos.js
+++ pantallas/contactos.js
@@ -1,2 +1,15 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
+
+const ContactosScreen = () => {
+    const [contactos, setContactos] = useState([]);
+    const [nombre, setNombre] = useState('');
+    const [telefono, setTelefono] = useState('');
+  
+    const agregarContacto = () => {
+      if (nombre && telefono) {
+        setContactos([...contactos, { id: Date.now().toString(), nombre, telefono }]);
+        setNombre('');
+        setTelefono('');
+      }
+    };
