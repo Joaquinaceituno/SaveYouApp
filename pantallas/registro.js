75068830f99fe71c36150ca45815a7556ec4c640
Pupghst <xiararojido04@gmail.com>
2023-11-13 18:57:10 -0300

    Poniendo mayuscula en la R de registro

--- pantallas/registro.js
+++ pantallas/registro.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function registro(){
+export default function Registro(){
   const [nombre, setNombre] = useState('');
   const [email, setEmail] = useState('');
   const [contrasena, setContrasena] = useState('');
