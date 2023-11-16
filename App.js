139c319ac825354f15eedfa860af5076c4e96c37
lautaro <lautaroolguin89@gmail.com>
2023-11-16 20:48:50 -0300

    haciendo la base de datos

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import Login from './pantallas/login';
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler'
+//firebase
 import appFirebase from './assets/credenciales';
+import {getAuth, onAuthStateChanged} from 'firebase/auth'
 
 export default function App() {
   return (
