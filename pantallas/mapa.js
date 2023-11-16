5c524b9d304ec0c72d430aff33bdd406dc0bee70
joaquin <joaquineaceituno@gmail.com>
2023-11-16 18:04:35 -0300

    estilos para el contenedor y el mapa

--- pantallas/mapa.js
+++ pantallas/mapa.js
@@ -51,4 +51,21 @@ export default function Mapa() {
             )
     }
 
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+    
+        map: {
+            width: '100%',
+            height: '100%'
+        }
+    
+    
+    
+    });
+
 
