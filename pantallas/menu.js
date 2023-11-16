7baf8d0e8ec21ccc9cd67704ac7a26acafe5e2a9
Alejo Angulo <angulojoaquin771@gmail.com>
2023-11-16 20:19:58 -0300

    se pone el contenedor y donde se definiran las pantallas

--- pantallas/menu.js
+++ pantallas/menu.js
@@ -2,3 +2,16 @@ import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return(
+    <NavigationContainer>
+      <Drawer.Navigator>
+        
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+
+
+}
