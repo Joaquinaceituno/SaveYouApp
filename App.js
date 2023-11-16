f2d202274f0baac929bbf3e4378027bc8f4b46f2
lautaro <lautaroolguin89@gmail.com>
2023-11-16 19:09:57 -0300

    tratando de conectar las pantallas

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './pantallas/login';
 import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler'
 
 export default function App() {
   return (
