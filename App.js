55fa93d3efe1cec8ba0280025844445f98a58fd4
lautaro <lautaroolguin89@gmail.com>
2023-11-16 20:40:40 -0300

    importando firebase

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import Login from './pantallas/login';
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler'
+import appFirebase from './assets/credenciales';
 
 export default function App() {
   return (
