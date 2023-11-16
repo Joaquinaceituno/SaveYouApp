3d7becbf8e232b77361412a1dc0f2e4240e85b2a
Alejo Angulo <angulojoaquin771@gmail.com>
2023-11-16 20:44:30 -0300

    se importan las pantallas

--- pantallas/menu.js
+++ pantallas/menu.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+import NovedadesScreen from './Novedades';
+import ContactosScreen from './contactos';
+import PerfilScreen from './Perfil';
 
 
 const Drawer = createDrawerNavigator();
