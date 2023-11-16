11d6fb678a2fb2e48eaa660fdaa313814d818393
Alejo Angulo <angulojoaquin771@gmail.com>
2023-11-16 20:34:37 -0300

    se definen las panatllas y se le cambian el nombre a algunos archivos

--- pantallas/menu.js
+++ pantallas/menu.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 
+
 const Drawer = createDrawerNavigator();
 
 function MyDrawer() {
