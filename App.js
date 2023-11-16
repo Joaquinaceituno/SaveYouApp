6b410f42030fdcf34157c5b458c81908edb3b937
Alejo Angulo <angulojoaquin771@gmail.com>
2023-11-16 18:25:07 -0300

    se hicieron unos cambios para probar la apĺicacion

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './navigation';
-import Login from './pantallas/login';
+
 
 export default function App() {
-  return (
-    <Login />
+  Menu (
+   
   )
 
 }
