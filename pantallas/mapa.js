2447a178ac6058d1265c2a92009d58e8cb10c20a
joaquin <joaquineaceituno@gmail.com>
2023-11-16 17:45:58 -0300

    se importan las dependencias necesarias

--- pantallas/mapa.js
+++ pantallas/mapa.js
@@ -1,10 +1,10 @@
-import react from 'react';
-import { View, Text } from 'react-native';
+import * as react from 'react';
+import * as location from 'expo-location';
+import { View, Text, StyleSheet } from 'react-native';
+import MapViewDirections from 'react-native-maps-directions';
+import MapView, {Marker, Polyline} from 'react-native-maps';
+import {GOOGLE_API_KEY} from '@env';
 
 export default function Mapa() {
-    return (
-        <View>
-            <Text>Mapa</Text>
-        </View>
-    )
+   
 }
