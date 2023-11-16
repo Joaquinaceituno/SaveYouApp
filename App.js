6fcdea4e3be70bda119934a42e04c5de7fc006ae
joaquin <joaquineaceituno@gmail.com>
2023-11-16 17:40:58 -0300

    se importan pantallas

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './navigation';
 import Login from './pantallas/login';
+import Contacto from './pantallas/contactos';
+import Registro from './pantallas/registro';
+
 
 export default function App() {
   return (
-    <Login />
+    <Login/>
   )
 
 }
