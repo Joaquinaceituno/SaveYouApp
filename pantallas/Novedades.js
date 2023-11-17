c945643d64406a467ad5bcce0f514747f56f7416
Alejo Angulo <angulojoaquin771@gmail.com>
2023-11-16 21:08:17 -0300

    se crea el const y se instalan las dependencias para que funcionen las etiquetas View y text

--- pantallas/Novedades.js
+++ pantallas/Novedades.js
@@ -1,12 +1,13 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from "react"
+import { View, Text } from "react-native"
 
 const NovedadesScreen = () => {
     return{
-        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-            <Text>Bienvenido a la pantalla de Novedades</Text>
-        </View>
+        
+            
 
     }
 
 }
+
+export default NovedadesScreen
