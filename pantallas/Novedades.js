d1d31547d23f108028e12bc1cf21bf765d77daed
Alejo Angulo <angulojoaquin771@gmail.com>
2023-11-16 21:00:53 -0300

    se crea el const, el return y se pone el View

--- pantallas/Novedades.js
+++ pantallas/Novedades.js
@@ -1,3 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+const NovedadesScreen = () => {
+    return{
+        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+            <Text>Bienvenido a la pantalla de Novedades</Text>
+        </View>
+
+    }
+
+}
