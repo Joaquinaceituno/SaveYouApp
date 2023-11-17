a973da7a788c9d1e83d0ae4385ea0cb3ff5f1f6c
lautaro <lautaroolguin89@gmail.com>
2023-11-17 12:34:42 -0300

    haciendo npm install

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import Login from './pantallas/login';
-
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler'
 //firebase
