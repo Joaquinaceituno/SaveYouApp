953191d1567e759aa87ca1d7fab5a55948baf91e
Joaquinaceituno <101936769+Joaquinaceituno@users.noreply.github.com>
2023-11-27 15:20:48 -0300

    Update mapa.js

--- pantallas/mapa.js
+++ pantallas/mapa.js
@@ -4,34 +4,19 @@ import { View, Text, StyleSheet } from 'react-native';
 import MapViewDirections from 'react-native-maps-directions';
 import MapView, {Marker, Polyline} from 'react-native-maps';
 import {GOOGLE_API_KEY} from '@env';
-<<<<<<< HEAD
+
 
 
 export default function Mapa() {
-    const [origin, setOrigin] = react.useState({
-=======
 
 export default function Mapa() {
     const [origin, setOrigin] = react.useState({ //ubicacion predeterminada de inicio
->>>>>>> 1bbb01758f1c0c521f6127c10ec5e8396de15e69
+
         latitude: -38.951890,
         longitude: -68.059173,
     }) 
 
-<<<<<<< HEAD
-    const [destination, setDestination]= react.useState({
-        latitude: -38.956585,
-        longitude: -68.058960,
-    })
     
-
-
-
-    return (
-        <View style={styles.container}>
-            
-            <MapView style={styles.map}
-=======
     const [destination, setDestination]= react.useState({ //ubicacion predeterminada de llegada
         latitude: -38.956585,
         longitude: -68.058960,
@@ -42,7 +27,7 @@ export default function Mapa() {
     }> 
             
             <MapView style={styles.map} //mapa
->>>>>>> 1bbb01758f1c0c521f6127c10ec5e8396de15e69
+
             initialRegion={{
                 latitude: origin.latitude,
                 longitude: origin.longitude,
@@ -50,29 +35,23 @@ export default function Mapa() {
                 longitudeDelta: 0.02,
             }}
             >
-<<<<<<< HEAD
-                <Marker 
-=======
+
                 <Marker //marcador de inicio
->>>>>>> 1bbb01758f1c0c521f6127c10ec5e8396de15e69
+
                 draggable={true}
                 coordinate={origin}
                 onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate) }
                 />
-<<<<<<< HEAD
-                <Marker 
-=======
+
                 <Marker //marcador de llegada
->>>>>>> 1bbb01758f1c0c521f6127c10ec5e8396de15e69
+
                 draggable={true}
                 coordinate={destination}
                 onDragEnd={(direction) => setDestination(direction.nativeEvent.coordinate)}
                 />
-<<<<<<< HEAD
-                <MapViewDirections
-=======
+
                 <MapViewDirections //ruta para ir de una direccion a otra
->>>>>>> 1bbb01758f1c0c521f6127c10ec5e8396de15e69
+
                     origin={origin}
                     destination={destination}
                     apikey={GOOGLE_API_KEY}
@@ -82,7 +61,7 @@ export default function Mapa() {
 
             </MapView>
         </View>
-<<<<<<< HEAD
+
     )
 }
 
@@ -102,27 +81,4 @@ const styles = StyleSheet.create({
 
 
 });
-=======
 
-            )
-    }
-
-    const styles = StyleSheet.create({ //estilos de contenedor y mapa
-        container: {
-            flex: 1,
-            backgroundColor: '#fff',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-    
-        map: {
-            width: '100%',
-            height: '100%'
-        }
-    
-    
-    
-    });
-
-
->>>>>>> 1bbb01758f1c0c521f6127c10ec5e8396de15e69
