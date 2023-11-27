51b7312a669586912014cb2a0fddb1cad68dfffc
joaquin <joaquineaceituno@gmail.com>
2023-11-27 14:50:24 -0300

    se agrega codigo funcional al repositorio

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Navigation from './navigation';
+import React from 'react';
 import Login from './pantallas/login';
+
+
+import Contacto from './pantallas/contactos';
+import Registro from './pantallas/registro';
+import MyDrawer from './pantallas/menu';
+import Navigation from './navigation';
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+
+
+
 
 export default function App() {
   return (
     <Login />
-  )
-
+  );
 }
+
+
 
--- pantallas/mapa.js
+++ pantallas/mapa.js
@@ -1,10 +1,72 @@
-import react from 'react';
-import { View, Text } from 'react-native';
+import * as react from 'react';
+import * as location from 'expo-location';
+import { View, Text, StyleSheet } from 'react-native';
+import MapViewDirections from 'react-native-maps-directions';
+import MapView, {Marker, Polyline} from 'react-native-maps';
+import {GOOGLE_API_KEY} from '@env';
+
 
 export default function Mapa() {
+    const [origin, setOrigin] = react.useState({
+        latitude: -38.951890,
+        longitude: -68.059173,
+    }) 
+
+    const [destination, setDestination]= react.useState({
+        latitude: -38.956585,
+        longitude: -68.058960,
+    })
+    
+
+
+
     return (
-        <View>
-            <Text>Mapa</Text>
+        <View style={styles.container}>
+            
+            <MapView style={styles.map}
+            initialRegion={{
+                latitude: origin.latitude,
+                longitude: origin.longitude,
+                latitudeDelta: 0.02,
+                longitudeDelta: 0.02,
+            }}
+            >
+                <Marker 
+                draggable={true}
+                coordinate={origin}
+                onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate) }
+                />
+                <Marker 
+                draggable={true}
+                coordinate={destination}
+                onDragEnd={(direction) => setDestination(direction.nativeEvent.coordinate)}
+                />
+                <MapViewDirections
+                    origin={origin}
+                    destination={destination}
+                    apikey={GOOGLE_API_KEY}
+                    strokeColor='blue'
+                    strokeWidth={6} 
+                />
+
+            </MapView>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    map: {
+        width: '100%',
+        height: '100%'
+    }
+
+
+
+});
